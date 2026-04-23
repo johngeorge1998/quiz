8adf5a02785c7f8d9fb0cc21794ecc4ad6069851
johnlinnk <john.george@linnk.com>
2026-04-23 14:55:34 +0530

    code format

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { AuthForm } from '@/components/auth';
+import { AuthForm } from "@/components/auth";
 
 export default function AuthPage() {
   return (
--- frontend/src/components/quiz/QuizProgress.tsx
+++ frontend/src/components/quiz/QuizProgress.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+"use client";
 
 interface QuizProgressProps {
   progress: number;
